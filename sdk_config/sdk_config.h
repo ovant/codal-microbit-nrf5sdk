a75ed976ebed47091b3ed25e4787a626843b9e29
Ovi <ovidiu.antal36@gmail.com>
2022-07-28 10:23:59 +0100

    added config options for scan

--- sdk_config/sdk_config.h
+++ sdk_config/sdk_config.h
@@ -5213,6 +5213,21 @@
 #define CLOCK_CONFIG_SOC_OBSERVER_PRIO 0
 #endif
 
+// <o> NRF_BLE_SCAN_BUFFER - Data length for an advertising set. 
+#ifndef NRF_BLE_SCAN_BUFFER
+#define NRF_BLE_SCAN_BUFFER 31
+#endif
+
+// <o> NRF_BLE_SCAN_SCAN_INTERVAL - Scanning interval. Determines the scan interval in units of 0.625 millisecond. 
+#ifndef NRF_BLE_SCAN_SCAN_INTERVAL
+#define NRF_BLE_SCAN_SCAN_INTERVAL 160
+#endif
+
+// <o> NRF_BLE_SCAN_SCAN_WINDOW - Scanning window. Determines the scanning window in units of 0.625 millisecond. 
+#ifndef NRF_BLE_SCAN_SCAN_WINDOW
+#define NRF_BLE_SCAN_SCAN_WINDOW 80
+#endif
+
 // <o> POWER_CONFIG_SOC_OBSERVER_PRIO  
 // <i> Priority with which SoC events are dispatched to the Power driver.
 
